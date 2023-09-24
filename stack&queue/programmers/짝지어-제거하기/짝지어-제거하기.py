b47fb760bf71f46020bdb03293c4196caea69f8d
devJS00 <110683103+devJS00@users.noreply.github.com>
2023-09-25 01:36:05 +0900

    Create 짝지어-제거하기.py

---
+++ stack&queue/programmers/짝지어-제거하기/짝지어-제거하기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = -1
+    stack = []
+    for i in s:
+        if stack != [] and stack[-1] == i: # 이번 문자가 스택 맨 위에 있는 문자와 같은 경우
+            stack.pop() # 이번 문자를 스택에 넣지 않고 맨 위에 있는 문자를 스택에서 꺼냄 -> 짝지어 제거
+        else: stack.append(i) # 문자를 스택에 넣음
+        
+    if stack == []: # 실행이 끝난 후 스택이 비어있으면
+        answer = 1 # 성공
+    else: answer = 0 # 아니면 실패
+    
+    return answer
