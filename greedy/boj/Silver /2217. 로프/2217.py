0490723bed3a08b2ee6af3a69cab20f74403460d
jisu-lee <110683103+devJS00@users.noreply.github.com>
2024-10-02 01:40:53 +0900

    Create 2217.py

---
+++ greedy/boj/Silver /2217. 로프/2217.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+weight = []
+result = []
+
+for i in range(n):
+    weight.append(int(input()))
+
+weight.sort(reverse=True)
+
+for i in range(n):
+    result.append(weight[i] * (i + 1))
+
+print(max(result))
