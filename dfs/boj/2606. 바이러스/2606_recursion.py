2a129e357a293efea866575c7a1bce9c1ef0864b
jisu-lee <110683103+devJS00@users.noreply.github.com>
2024-10-05 12:52:58 +0900

    Update 2606_recursion.py

--- dfs/boj/2606. 바이러스/2606_recursion.py
+++ dfs/boj/2606. 바이러스/2606_recursion.py
@@ -1,24 +1,27 @@
 import sys
 sys.setrecursionlimit(10**4)
 
-comp = int(input()) # 컴퓨터의 수
-pair = int(input()) # 컴퓨터 쌍의 수
-graph = [[] for i in range(comp+1)]
-visited = [0]*(comp+1) # 방문 정보 표현할 리스트
+comp_n = int(input())  # 컴퓨터의 수
+pair_n = int(input())  # 컴퓨터 쌍의 수
+
+graph = [[] for _ in range(comp_n + 1)]
+visited = [0] * (comp_n + 1)  # 방문 정보 표현할 리스트
 answer = 0
 
-for i in range(pair):
-    a, b = map(int, input().split()) # 연결되어 있는 컴퓨터의 번호 쌍
-    graph[a].append(b) # graph의 a번째 리스트에 b를 넣는 것으로 a와 b가 연결되어 있다는 것 표현
-    graph[b].append(a) # 같은 방식으로 b의 입장에서도 a와 연결되어 있다는 것 표현
+for i in range(pair_n):
+    c1, c2 = map(int, input().split())
+    graph[c1].append(c2)  # graph의 c1번째 리스트에 c2를 넣는 것으로 c1와 c2가 연결되어 있다는 것 표현
+    graph[c2].append(c1)  # 같은 방식으로 c2의 입장에서도 c1과 연결되어 있다는 것 표현
+
 
 def dfs(i):
     global answer
-    visited[i] = 1 # 방문 처리
-    for j in graph[i]:
-        if visited[j] == 0: # 방문하지 않은 노드인 경우
-            dfs(j) # dfs 함수 호출
+    visited[i] = 1
+    for g in graph[i]:
+        if visited[g] == 0:  # 방문하지 않은 노드인 경우
+            dfs(g)  # dfs 함수 호출
             answer += 1
+
 
 dfs(1)
 print(answer)
