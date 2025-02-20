baa6411d4ce0865b306cb6c2635c8c227b04784b
Jisu Lee <110683103+devJS00@users.noreply.github.com>
2025-02-20 16:10:40 +0900

    Create 15651.java

---
+++ backtracking/boj/silver/15651. N과 M (3)/15651.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static ArrayList<Integer> selected = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();  // StringBuilder 사용하지 않으면 시간 초과 발생
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        backtracking();
+
+        System.out.print(sb);
+        br.close();
+    }
+
+    private static void backtracking() {
+        if (selected.size() == M) {
+            for (int s : selected) {
+                sb.append(s).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            selected.add(i);
+            backtracking();
+            selected.remove(selected.size() - 1);
+        }
+    }
+}
